381cef04634fbb40ca0dddb7eba72dc1182280fc
Clifton Craig <clifton.craig@gmail.com>
2020-07-10 23:54:27 -0700

    Add more secret sauce

--- webhook.js
+++ webhook.js
@@ -25,8 +25,12 @@ function getHandler(req, res) {
 
 function postHandler(req, res) {
   const body = req.body;
-  console.log(body);
-  res.status(200).json(body);
+  if(req.query.token && req.query.token === process.env.MY_TOKEN) {
+    res.status(200).json({authorized:true})
+  } else {
+    console.log(body);
+    res.status(200).json(body);  
+  }
 }
 
 module.exports = (req, res) => {
